aa3b3d75d7e4d46e4fd081a54d20626f8b59a00f
Moses Palmér <mosespalmer@gmail.com>
2012-09-11 16:44:12 +0200

    Added stub for Wall class

--- lib/maze/__init__.py
+++ lib/maze/__init__.py
@@ -1,13 +1,45 @@
+class Wall(object):
+    """
+    A reference to the wall of a room.
+
+    A wall has an index and a direction.
+      * The index is its position in the list (LEFT, UP, RIGHT, DOWN).
+      * The direction is a direction vector for the wall; up and right are
+        positive directions.
+    """
+
+    # Define the walls; this will also add the class variables mapping wall name
+    # to its value
+    DIRECTIONS = []
+    NAMES = []
+    WALLS = []
+    for i, (name, hdir, vdir) in enumerate((
+            ('LEFT', -1, 0),
+            ('UP', 0, 1),
+            ('RIGHT', 1, 0),
+            ('DOWN', 0, -1))):
+        locals()[name] = i
+        DIRECTIONS.append((hdir, vdir))
+        NAMES.append(name.lower())
+        WALLS.append(i)
+
+    def __init__(self, room_pos, wall):
+        self.room_pos = room_pos
+        self.wall = wall
+
+
 class Room(object):
     """
     A room is a part of the maze.
+
+    A room has all walls defined in Wall, and a concept of doors on the walls.
     """
 
     def __init__(self):
         """
         Creates a new room.
         """
-        pass
+        self.doors = set()
 
 
 class Maze(object):
--- tests/suites/maze_tests.py
+++ tests/suites/maze_tests.py
@@ -2,6 +2,29 @@ from tests import *
 from maze import *
 
 @test
+def Wall_fields():
+    """Tests that Wall contains the expected fields"""
+    walls = set()
+
+    assert_eq(len(Wall.NAMES), 4)
+
+    for a in (name.upper() for name in Wall.NAMES):
+        assert hasattr(Wall, a), \
+            'Wall.%s is undefined' % a
+
+        w = getattr(Wall, a)
+        assert not w in walls, \
+            'Wall.%s has a previously set value' % a
+
+        walls.add(w)
+
+    assert_eq(len(Wall.WALLS), len(Wall.DIRECTIONS))
+    assert_eq(len(Wall.DIRECTIONS), len(set(Wall.DIRECTIONS)))
+    assert_eq(len(Wall.WALLS), len(Wall.NAMES))
+    assert_eq(len(Wall.NAMES), len(set(Wall.NAMES)))
+
+
+@test
 def Maze_width_and_height():
     """Tests that the width and height properties are correct"""
     maze1 = Maze(10, 20)
