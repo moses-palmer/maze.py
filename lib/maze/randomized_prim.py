8b6239a9a939fa30624fdfdc73d5eb52fc4b4b1d
Moses Palmér <mosespalmer@gmail.com>
2012-10-08 23:16:12 +0200

    Use Maze.set_door in randomized_prim

--- lib/maze/randomized_prim.py
+++ lib/maze/randomized_prim.py
@@ -27,7 +27,7 @@ def initialize(maze, randomizer):
         # Is this the first time we visit this room?
         if not maze[next_room_pos]:
             # Add a door to the wall
-            maze[wall.room_pos:next_room_pos] = True
+            maze.set_door(wall.room_pos, wall, True)
 
             # Add all walls of the new room except those on the edge and those
             # leading to rooms already visited
