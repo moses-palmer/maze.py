1f820a2d1fb6e8487235523002cacc50f9769577
Moses Palmér <mosespalmer@gmail.com>
2012-11-16 16:29:33 +0100

    Corrected and updated documentation for Maze

--- lib/maze/quad.py
+++ lib/maze/quad.py
@@ -3,38 +3,49 @@ import math
 from . import BaseWall, BaseMaze
 
 class QuadWall(BaseWall):
-    # Define the walls; this will also add the class variables
-    # mapping wall name to its value
+    # The angles for each corner
     _ANGLES = []
+
+    # The directions for each wall
     _DIRECTIONS = []
+
+    # The names of the walls
     NAMES = []
+
+    # The list of walls; this is the list [0, 1, 2... N]
     WALLS = []
+
+    # The scale factor when converting maze coordinates to physical coordinates
+    MULTIPLICATOR = 2.0 / math.sqrt(2.0)
 
     __slots__ = tuple()
 
     start_angle = (5 * math.pi) / 4
     data = (
-        ('LEFT', -1, 0),
-        ('UP', 0, 1),
-        ('RIGHT', 1, 0),
-        ('DOWN', 0, -1))
-    for i, (name, hdir, vdir) in enumerate(data):
+        ('LEFT', (-1, 0)),
+        ('UP', (0, 1)),
+        ('RIGHT', (1, 0)),
+        ('DOWN', (0, -1)))
+    for i, (name, dir1) in enumerate(data):
         locals()[name] = i
+
         next_angle = _ANGLES[-1] - 2 * math.pi / len(data) \
             if _ANGLES else start_angle
-
         while next_angle < 0.0:
             next_angle += 2 * math.pi
         _ANGLES.append(next_angle)
-        _DIRECTIONS.append((hdir, vdir))
+
+        _DIRECTIONS.append(dir1)
+
         NAMES.append(name.lower())
         WALLS.append(i)
-
-    MULTIPLICATOR = 2.0 / math.sqrt(2.0)
 
 class Maze(BaseMaze):
     """
     This is a maze with square rooms.
+
+    This is the traditional maze. Maze coordinates correspond to physical
+    coordinates after a simple scale operation.
     """
     Wall = QuadWall
 
