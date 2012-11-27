c3fe0257046027bdc9647559d4fc75b324e09b7d
Moses Palmér <mosespalmer@gmail.com>
2012-11-27 21:50:14 +0100

    Moved print_maze to separate file

--- app/amaze/__init__.py
+++ app/amaze/__init__.py
@@ -15,70 +15,7 @@ from maze.tri import TriMaze
 from maze.hex import HexMaze
 from maze.randomized_prim import initialize
 
-def print_maze(maze, solution, wall_char, path_char, floor_char, room_size):
-    if len(maze.Wall.WALLS) != 4:
-        print 'This maze cannot be printed as it is not square'
-        return
-
-    def output(s):
-        sys.stdout.write(s)
-
-    # Iterate over all rows and make sure to start with the last one to maintain
-    # the orientation of the maze
-    for y in reversed(xrange(maze.height)):
-        # Print one line for each height unit of a room
-        for line in xrange(room_size[1]):
-            # Calculate the vertical offset of the neighbouring room
-            dy = 1 if not line else -1 if line == room_size[1] - 1 else 0
-            for x in xrange(maze.width):
-                # Print one line of the current room for every room in the
-                # current row
-                if line == 0 or line == room_size[1] - 1:
-                    # This is the first or the last line
-                    output(''.join((
-                        # The left wall
-                        wall_char,
-
-                        # The opening or top/bottom wall
-                        (path_char if \
-                            int(maze.Wall.from_direction((x, y), (0, dy))) \
-                                in maze[x, y] \
-                            and (x, y) in solution \
-                            and (x, y + dy) in solution
-                        else floor_char \
-                            if int(maze.Wall.from_direction((x, y), (0, dy))) \
-                                in maze[x, y]
-                        else wall_char) * (room_size[0] - 2),
-
-                        # The right wall
-                        wall_char)))
-                else:
-                    # This is a line in the middle of a room
-                    output(''.join((
-                        # The left opening or wall
-                        path_char if int(maze.Wall.from_direction(
-                                (x, y), (-1, 0))) in maze[x, y] \
-                            and (x, y) in solution \
-                            and (x - 1, y) in solution
-                        else floor_char if \
-                            int(maze.Wall.from_direction((x, y), (-1, 0))) \
-                                in maze[x, y]
-                        else wall_char,
-
-                        # The center
-                        (path_char if (x, y) in solution \
-                        else floor_char) * (room_size[0] - 2),
-
-                        # The right opening or wall
-                        path_char if int(maze.Wall.from_direction(
-                                (x, y), (1, 0))) in maze[x, y] \
-                            and (x, y) in solution \
-                            and (x + 1, y) in solution
-                        else floor_char if \
-                            int(maze.Wall.from_direction((x, y), (1, 0))) \
-                                in maze[x, y]
-                        else wall_char)))
-            output('\n')
+from terminal import print_maze
 
 
 def calculate_bounds(maze):
