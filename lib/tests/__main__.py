43a6d6c74ad96bd89457000f3023b7eb10bea857
Moses Palmér <mosespalmer@gmail.com>
2014-05-19 14:47:47 +0200

    Moved tests runner script into module

--- lib/tests/__main__.py
+++ lib/tests/__main__.py
@@ -1,21 +1,20 @@
-#!/usr/bin/env python
-
 import os
 import sys
 
 import tests
 from tests.suites import *
 
+
 failures = tests.run()
 if failures is None:
     print('Test suite was cancelled by setup')
     sys.exit(-1)
 
+
 print('')
 print('All test suites completed with %d failed tests' % len(failures))
 if failures:
-    print('Failed tests:\n%s' % '\n'.join(
+    sys.stderr.write('Failed tests:\n%s\n' % '\n'.join(
         '\t%s - %s' % (test.name, test.message)
             for test in failures))
 sys.exit(len(failures))
-
